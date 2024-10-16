7131a10ba429376163cd234da2149340ec60c496
Elias Weitfle <113947042+witflee@users.noreply.github.com>
2024-10-16 13:39:44 -0400

    add stores file skeleton

---
+++ src/lib/stores.js
@@ -0,0 +1,9 @@
+import { writable } from 'svelte/store';
+
+export const temp = writable();
+export const pressure = writable();
+export const waterAmt = writable();
+export const beans = writable();
+export const currentBrew = writable();
+export const timer = writable();
+export const caffAmt = writable();
