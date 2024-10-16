11459f8bfb856554003ff580f2a50163cd009eeb
Nathan Nguyen <nathanpn2003@gmail.com>
2024-10-16 12:52:03 -0400

    added shadcn

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,21 @@
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
+<script>import "./app.css";
+import svelteLogo from './assets/svelte.svg'
+import viteLogo from '/vite.svg'
+import Counter from './lib/Counter.svelte'</script>
 
 <main>
   <div>
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
+      <img src="{viteLogo}" class="logo" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      <img src="{svelteLogo}" class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
   <h1>Vite + Svelte</h1>
 
   <div class="card">
-    <Counter />
+    <Counter></Counter>
   </div>
 
   <p>
@@ -27,6 +26,7 @@
     Click on the Vite and Svelte logos to learn more
   </p>
 </main>
+
 
 <style>
   .logo {
